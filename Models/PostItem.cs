69d7c5b8171daa0182f50842399c92639edf294b
Prem Rajah <prajah@ctlondon.com>
2019-05-17 09:31:22 +0100

    Add project files.

---
+++ Models/PostItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKGovNews.Models
+{
+    public class PostItem
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Link { get; set; }
+        public DateTime Updated { get; set; }
+
+    }
+}
