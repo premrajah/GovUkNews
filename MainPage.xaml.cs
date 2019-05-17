03f8c7573d8846eb8f936389a50d4d9913710b4b
Prem Rajah <prajah@ctlondon.com>
2019-05-17 17:37:31 +0100

    UK Gov start

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UKGovNews.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,7 @@ namespace UKGovNews
         public MainPage()
         {
             this.InitializeComponent();
+            this.DataContext = new MainViewModel();
         }
     }
 }
---
+++ Models/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKGovNews.Models
+{
+    public class MainViewModel
+    {
+        
+        public readonly string CopyrightText = "Copyright belongs to respective copyright holders, Information publicly avaliable on website gov.uk";
+        public List<PostItem> PostItems { get; set; }
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
